f21f308def75c08a0efaa263bf84b71d5d0f2070
sathiyapriyanofficial <sathiyapriyan.official@gmail.com>
2025-07-27 14:13:22 +0530

    static & non static var

---
+++ C For Programming/1.2.staticvar.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// Function declarations
+void static_func();
+void non_static_func();
+
+int main()
+{
+  static_func();
+  static_func();
+  static_func();
+  non_static_func();
+  non_static_func();
+  non_static_func();
+  non_static_func();
+  return 0;
+}
+
+// Function with static variable
+void static_func()
+{
+  static int count = 4;
+  count = count + 1;
+  printf("The static value of count is: %d\n", count);
+}
+
+// Function with non-static variable
+void non_static_func()
+{
+  int count = 4;
+  count = count + 1;
+  printf("The non-static value of count is: %d\n", count);
+}
