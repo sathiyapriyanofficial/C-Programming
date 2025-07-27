f9a25ab38cca0c569a05ac5191c35114e23e4856
sathiyapriyanofficial <sathiyapriyan.official@gmail.com>
2025-07-27 13:39:01 +0530

    auto variable

---
+++ C For Programming/1.1.autovar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+  // Automatic variable - local variables are by default 'auto'
+  auto int x = 10; // 'auto' is optional here
+  printf("The Value of X is %d\n", x);
+
+  return 0;
+}
